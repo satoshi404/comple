bc10fa967294560896afea913c26f8cc8c13d1e2
Satoshi <alwaysindiscipline@gmail.com>
2024-10-30 19:50:37 -0300

    lets_go_again

--- src/lexer.c
+++ src/lexer.c
@@ -27,6 +27,10 @@ TVec* lexer_tokenize(Lexer* lexer) {
             tvec_push(tokens, token_init(TT_VAR_, 0, "VAR"));
             lexer->lptr+=3;
             continue;
+        }  else if (strncmp(&lexer->src[lexer->lptr], "puts", 4) == 0) {
+            tvec_push(tokens, token_init(TT_FUNCTION_, 0, "FUNCTION"));
+            lexer->lptr+=4;
+            continue;
         } else if (isalpha(c)) {
             char* name = (char*) malloc(sizeof(char));
             size_t cap = 1;
@@ -51,10 +55,6 @@ TVec* lexer_tokenize(Lexer* lexer) {
                 lexer->lptr++;
             }
             tvec_push(tokens, token_init(TT_INT_, value, "INT"));
-            continue;
-        } else if (strncmp(&lexer->src[lexer->lptr], "puts", 4) == 0) {
-            tvec_push(tokens, token_init(TT_FUNCTION_, 0, "FUNCTION"));
-            lexer->lptr+=4;
             continue;
         } else if (c == '+') {
             tvec_push(tokens, token_init(TT_PLUS_, 0, "PLUS"));
